51fbf8c2143636e9a6d230a402e5604715ad653b
notification <tzlchnnotification@gmail.com>
2012-06-13 10:59:37 +0530

    my first commit

---
+++ EGitEx/src/com/test/Example.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class Example {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome");		
+	}
+
+}
