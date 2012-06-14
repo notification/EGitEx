cdedf0a71b96016b2b02869d4ec779d543ad1a2d
notification <tzlchnnotification@gmail.com>
2012-06-14 18:33:09 +0530

    new line added from web client

--- EGitEx/src/com/test/Example.java
+++ EGitEx/src/com/test/Example.java
@@ -7,7 +7,7 @@ public class Example {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome");		
+		System.out.println("Welcome to the Eclispe Test Project");		
 	}
 
 }
